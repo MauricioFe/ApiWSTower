6fc95a8b26d15f07b581c034dcee20b3863657f4
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-14 09:51:40 -0300

    Criação dos models

---
+++ WSTowerApi/WSTowerApi/Models/Relatos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WSTowerApi.Models
+{
+    public class Relatos
+    {
+        public int id { get; set; }
+        public string relato { get; set; }
+        public string imagem { get; set; }
+        public decimal latitude { get; set; }
+        public decimal longitude { get; set; }
+        public int usuario_id { get; set; }
+        [ForeignKey("id")]
+        public Usuario usuario { get; set; }
+    }
+}
