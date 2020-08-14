9418d56cc5254ffa91686224746cadac90278a03
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-14 10:54:09 -0300

    Registrando o context

--- WSTowerApi/WSTowerApi/Startup.cs
+++ WSTowerApi/WSTowerApi/Startup.cs
@@ -27,7 +27,8 @@ namespace WSTowerApi
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {            
+        {
+            services.AddDbContext<WsTowerContext>(option => option.UseSqlServer(Configuration.GetConnectionString("conn")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
