fe62dca4a761b94b3ad1340a43f963e5015cd4dc
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-14 09:58:34 -0300

    Criação das interfaces

---
+++ WSTowerApi/WSTowerApi/Repository/IUsuarioRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSTowerApi.Models;
+
+namespace WSTowerApi.Repository
+{
+    interface IUsuarioRepository
+    {
+        IEnumerable<Funcao> GetAll();
+        Funcao Find(int id);
+        void Add(Funcao funcao);
+        void Update(Funcao funcao);
+        void Remove(int id);
+    }
+}
