f38c3368023d1ed85fc25f586c9abb40772702ba
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-08-17 16:20:45 -0300

    Criação das model

---
+++ WSTowerApi/WSTowerApi/Data/WsTowerContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSTowerApi.Models;
+
+namespace WSTowerApi.Data
+{
+    public class WsTowerContext : DbContext
+    {
+        public WsTowerContext(DbContextOptions<WsTowerContext> options): base (options)
+        {
+
+        }
+
+        public DbSet<Funcao> Funcao{ get; set; }
+        public DbSet<Usuario> Usuario{ get; set; }
+        public DbSet<Relatos> Relatos{ get; set; }
+    }
+}
